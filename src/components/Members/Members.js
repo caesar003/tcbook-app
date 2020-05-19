3a544891ea3976cde8b8789845f42e50f6a14c20
caesar003 <caesarmuksid@gmail.com>
2020-05-19 12:48:49 +0800

    updating readme

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Members = () => {
   return (
-    <p>this is member page</p>
+    <>
+      <p>this is member page</p>
+      <p>which later I intend to fill this with list of all registered members</p>
+    </>
   )
 }
 
